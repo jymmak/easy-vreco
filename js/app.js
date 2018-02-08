3119700f78e847302b95d0e7e4a8f12d84ed4b03
jymmak <henryfrancisco_fernando@hotmail.com>
2018-02-08 01:11:20 -0500

    modificando funcionalidda

--- js/app.js
+++ js/app.js
@@ -1,44 +1,38 @@
-var map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 7,
-  center: { lat: -12.020651498087096, lng: -77.0349046 },
-  mapTypeControl: false,
-  zoomControl: false,
-  streetViewControl: false
-});
-
-function buscar() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-  }
-}
-
-document.getElementById("locat").addEventListener("click", buscar);
-var latitud, longitud;
-var icons = 'assets/images/bici.png';
-
-var funcionExito = function (posicion) {
-  latitud = posicion.coords.latitude;
-  longitud = posicion.coords.longitude;
-
-  var miUbicacion = new google.maps.Marker({
-    position: { lat: latitud, lng: longitud },
-    animation: google.maps.Animation.DROP,
-    map: map,
-    icon: icons
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,//zoom representa el nivel de profundidad de nuestro mapa, entre más zoom más localizado se verá.
+    center: { lat: -12.020651498087096, lng: -77.0349046 },//center contiene la longitud y latitud en que queremos que se muestre nuestro mapa
+    mapTypeControl: false,
+    zoomControl: false,
+    streetViewControl: false
   });
 
-  map.setZoom(17);
-  map.setCenter({ lat: latitud, lng: longitud });
-}
+  function buscar() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);//getCurrentPosition permite al usuario obtener su ubicación actual, uncionExito se ejecuta solo cuando el usuario comparte su ubicación, mientras que funcionError se ejecuta cuando se produce un error en la geolocalización
+    }
+  }
 
-var funcionError = function (error) {
-  alert("tenemos un problema con encontrar tu ubicación");
-}
+  document.getElementById("locat").addEventListener("click", buscar);
+  var latitud, longitud;
 
-directionsDisplay.setMap(map);
-var onChangeHandler = function () {
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-};
-route.addEventListener('click', onChangeHandler);
 
+  var funcionExito = function (posicion) {//var funcionExito, con el que obtendremos nuestra latitud o longitud y además crearemos un marcador de nuestra ubicación.
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+    var miUbicacion = new google.maps.Marker({
+      position: { lat: latitud, lng: longitud },
+      animation: google.maps.Animation.DROP,
+      map: map
+
+    });
+
+    map.setZoom(17);
+    map.setCenter({ lat: latitud, lng: longitud });
+  }
+
+  var funcionError = function (error) {//funcionError con un mensaje para el usuario, en caso de que nuestra geolocalización falle.
+    alert("tenemos un problema con encontrar tu ubicación");
+  }
 }
