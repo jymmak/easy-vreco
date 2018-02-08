6a2ddc8677344a0b9a9a171026b8d452222a3da9
jymmak <henryfrancisco_fernando@hotmail.com>
2018-02-08 01:28:41 -0500

    modificando js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 function initMap() {
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,//zoom representa el nivel de profundidad de nuestro mapa, entre más zoom más localizado se verá.
-    center: { lat: -12.020651498087096, lng: -77.0349046 },//center contiene la longitud y latitud en que queremos que se muestre nuestro mapa
+    zoom: 7,
+    center: { lat: -12.020651498087096, lng: -77.0349046 },
     mapTypeControl: false,
     zoomControl: false,
     streetViewControl: false
@@ -9,30 +12,37 @@ function initMap() {
 
   function buscar() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);//getCurrentPosition permite al usuario obtener su ubicación actual, uncionExito se ejecuta solo cuando el usuario comparte su ubicación, mientras que funcionError se ejecuta cuando se produce un error en la geolocalización
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
     }
   }
 
   document.getElementById("locat").addEventListener("click", buscar);
   var latitud, longitud;
+  var icons = 'assets/images/bici.png';
 
-
-  var funcionExito = function (posicion) {//var funcionExito, con el que obtendremos nuestra latitud o longitud y además crearemos un marcador de nuestra ubicación.
+  var funcionExito = function (posicion) {
     latitud = posicion.coords.latitude;
     longitud = posicion.coords.longitude;
 
     var miUbicacion = new google.maps.Marker({
       position: { lat: latitud, lng: longitud },
       animation: google.maps.Animation.DROP,
-      map: map
-
+      map: map,
+      icon: icons
     });
 
     map.setZoom(17);
     map.setCenter({ lat: latitud, lng: longitud });
   }
 
-  var funcionError = function (error) {//funcionError con un mensaje para el usuario, en caso de que nuestra geolocalización falle.
+  var funcionError = function (error) {
     alert("tenemos un problema con encontrar tu ubicación");
   }
+
+  directionsDisplay.setMap(map);
+  var onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  route.addEventListener('click', onChangeHandler);
+
 }
